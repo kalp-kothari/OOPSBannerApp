f8d3384f8ef984c75dd9e676d477f4f826f3b45c
kalp-kothari <kalpkothari38@gmail.com>
2026-02-17 14:58:53 +0530

    UC2: Implement ASCII banner OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,22 @@
+/**
+ * OOPSBannerApp
+ * UC2 - Prints "OOPS" as a 7-line ASCII banner.
+ *
+ * @author Kalp
+ * @version 2.0
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("   *******     *******   *******   ********");
+        System.out.println("  **/////**   **/////** /**////** **////// ");
+        System.out.println(" **     //** **     //**/**   /**/**       ");
+        System.out.println("/**      /**/**      /**/******* /*********");
+        System.out.println("/**      /**/**      /**/**////  ////////**");
+        System.out.println("//**     ** //**     ** /**             /**");
+        System.out.println(" //*******   //*******  /**       ******** ");
+	System.out.println("  ///////     ///////   //       ////////  ");
+
     }
 }
