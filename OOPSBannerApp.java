f4d65150c3394266807fa1238d936d07772a76c5
kalp-kothari <kalpkothari38@gmail.com>
2026-02-26 15:02:32 +0530

    UC4: Printing Banner with Loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,19 @@
-/**
- * OOPSBannerApp
- * UC2 - Prints "OOPS" as a 7-line ASCII banner.
- *
- * @author Kalp
- * @version 2.0
- */
-import java.util.*;
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String s,z;
-	Scanner sc = new Scanner (System.in);
-	System.out.println("Enter the strings to join: ");
-	s = sc.nextLine();
-	z = sc.nextLine();
-	String k = String.join(" ", s, z);
-	System.out.println("Output: " + k);
+        String[] k = {
+	"   *******     *******   *******   ********",
+        "  **/////**   **/////** /**////** **////// ",
+        " **     //** **     //**/**   /**/**       ",
+        "/**      /**/**      /**/******* /*********",
+        "/**      /**/**      /**/**////  ////////**",
+        "//**     ** //**     ** /**             /**",
+        " //*******   //*******  /**       ******** ",
+	"  ///////     ///////   //       ////////  "};
+
+	for (int i = 0; i < k.length; i++) {
+            System.out.println(k[i]);
+	}
     }
 }
