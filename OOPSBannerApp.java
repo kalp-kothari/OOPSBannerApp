8def870bc68e5fb9237e579a1d749a2ba03f17a6
kalp-kothari <kalpkothari38@gmail.com>
2026-03-03 15:15:08 +0530

    UC6: Banner Print with DRY

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,62 @@
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
+        String[] o1 = buildO();
+        String[] o2 = buildO();
+        String[] p  = buildP();
+        String[] s  = buildS();
+        String[] banner = new String[o1.length];
+        for (int i = 0; i < banner.length; i++) {
+            banner[i] = String.join("  ", o1[i], o2[i], p[i], s[i]);
+        }
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
 
-        String banner = String.join("\n",
-		"   *******     *******   *******   ********",
-		"  **/////**   **/////** /**////** **////// ",
-		"**     //** **     //**/**   /**/**     ",
-		"/**      /**/**      /**/******* /*********",
-		"/**      /**/**      /**/**////  ////////**",
-		"//**     ** //**     ** /**             /**",
-		" //*******   //*******  /**       ******** ",
-		"  ///////     ///////   //       //////// "
-	);
+    private static String[] buildO() {
+        return new String[] {
+            "   *******  ",
+            "  **/////** ",
+            " **     //**",
+            "/**      /**",
+            "/**      /**",
+            "//**     ** ",
+            " //*******  ",
+            "  ///////   "
+        };
+    }
 
-        System.out.println(banner);
+    private static String[] buildP() {
+        return new String[] {
+            " *******  ",
+            "/**//**/**",
+            "/**  /**  ",
+            "/******** ",
+            "/**////   ",
+            "/**       ",
+            "/**       ",
+            "//        "
+        };
+    }
+
+    private static String[] buildS() {
+        return new String[] {
+            " ******** ",
+            "**//////  ",
+            "/**       ",
+            "/*********",
+            "////////**",
+            "       /**",
+            " ******** ",
+            " ////////  "
+        };
     }
 }
+
+
+
+
+
+
+
+
