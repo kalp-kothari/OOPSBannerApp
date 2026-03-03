3dc35ecebb07b74081541d7e15fecf75ed5d093f
kalp-kothari <kalpkothari38@gmail.com>
2026-03-03 15:48:45 +0530

    UC7: Printing Banner With Letter Functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,62 +1,77 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String[] o1 = buildO();
-        String[] o2 = buildO();
-        String[] p  = buildP();
-        String[] s  = buildS();
-        String[] banner = new String[o1.length];
+        String word = "OOPS";
+        String[] banner = new String[8];
         for (int i = 0; i < banner.length; i++) {
-            banner[i] = String.join("  ", o1[i], o2[i], p[i], s[i]);
+            banner[i] = "";
+        }
+        for (char ch : word.toCharArray()) {
+            CharacterPatternMap pattern = CharacterPatternMap.getPattern(ch);
+            if (pattern != null) {
+                String[] rows = pattern.getPattern();
+                for (int i = 0; i < rows.length; i++) {
+                    banner[i] = banner[i] + rows[i] + "   ";
+                }
+            }
         }
         for (String line : banner) {
             System.out.println(line);
         }
     }
 
-    private static String[] buildO() {
-        return new String[] {
-            "   *******  ",
-            "  **/////** ",
-            " **     //**",
-            "/**      /**",
-            "/**      /**",
-            "//**     ** ",
-            " //*******  ",
-            "  ///////   "
-        };
-    }
+    static class CharacterPatternMap {
+        private char character;
+        private String[] pattern;
 
-    private static String[] buildP() {
-        return new String[] {
-            " *******  ",
-            "/**//**/**",
-            "/**  /**  ",
-            "/******** ",
-            "/**////   ",
-            "/**       ",
-            "/**       ",
-            "//        "
-        };
-    }
+        private CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
 
-    private static String[] buildS() {
-        return new String[] {
-            " ******** ",
-            "**//////  ",
-            "/**       ",
-            "/*********",
-            "////////**",
-            "       /**",
-            " ******** ",
-            " ////////  "
+        public String[] getPattern() {
+            return pattern;
+        }
+
+        private static final CharacterPatternMap[] REGISTRY = {
+            new CharacterPatternMap('O', new String[]{
+                "   *******  ",
+                "  **/////** ",
+                " **     //**",
+                "/**      /**",
+                "/**      /**",
+                "//**     ** ",
+                " //*******  ",
+                "  ///////   "
+            }),
+            new CharacterPatternMap('P', new String[]{
+                "   *******",
+                " /**////**",
+                " /**   /**",
+                " /******* ",
+                " /**////  ",
+                " /**      ",
+                " /**      ",
+                " //       "
+            }),
+            new CharacterPatternMap('S', new String[]{
+                "   ********",
+                " **//////  ",
+                " /**       ",
+                " /*********",
+                " ////////**",
+                "        /**",
+                " ********  ",
+                " ////////  "
+            })
         };
+
+        public static CharacterPatternMap getPattern(char ch) {
+            for (CharacterPatternMap entry : REGISTRY) {
+                if (entry.character == ch) {
+                    return entry;
+                }
+            }
+            return null;
+        }
     }
 }
-
-
-
-
-
-
-
-
