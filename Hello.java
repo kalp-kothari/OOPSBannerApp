db59312f7a2ace14f38c995026752ffc1aaaad05
kalp-kothari <kalpkothari38@gmail.com>
2026-03-22 16:14:03 +0530

    UC2: Print with Command Line Argument

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 class Hello {
-	void main(String Args[]) {
-		System.out.println("Hello World!");
+	void main(String args[]) {
+		if (args.length >0) {
+			String name = args[0];
+		System.out.println("Hello, "+ name + "!");
+		}
+	else
+		System.out.println("Hello, World!");
 	}
 }
