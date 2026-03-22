2e1ce0e5417b76f811c816c0dab10ad93996d8b0
kalp-kothari <kalpkothari38@gmail.com>
2026-03-22 16:41:20 +0530

    UC4: Print with Multiple Command Line Argument

--- Hello.java
+++ Hello.java
@@ -1,8 +1,18 @@
 class Hello {
 	void main(String args[]) {
-		String name = "World";
-		if (args.length >0)
-			name = args[0];
+		StringBuilder nameBuilder = new StringBuilder();
+		String name = " ";
+		if (args.length >0) {
+		for (int i = 0; i < args.length; i++) {
+			nameBuilder.append(args[i]);
+			if (i<args.length-1) {
+				nameBuilder.append(", ");
+			}
+			name = nameBuilder.toString();
+		}
+		}
+		else
+			name = "World";
 		System.out.println("Hello, "+ name + "!");
 	}
 }
