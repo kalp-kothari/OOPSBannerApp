3ba6fab31bb686b0cdc73beff2ef4b73e7f1e620
kalp-kothari <kalpkothari38@gmail.com>
2026-03-22 16:29:20 +0530

    UC3: Print with Command Line Argument and Default

--- Hello.java
+++ Hello.java
@@ -1,10 +1,8 @@
 class Hello {
 	void main(String args[]) {
-		if (args.length >0) {
-			String name = args[0];
+		String name = "World";
+		if (args.length >0)
+			name = args[0];
 		System.out.println("Hello, "+ name + "!");
-		}
-	else
-		System.out.println("Hello, World!");
 	}
 }
